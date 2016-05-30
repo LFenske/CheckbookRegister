7c0696d091b8bc255d8ab86e261f76377d4103db
Larry Fenske <github@towanda.com>
2016-05-29 23:17:58 -0600

    In script.js:   Change automigrate to autocreate.   Change create to findOrCreate. This allows the server to be restarted without wiping Customer and without creating duplicate admin role and mapping.
    In model-config.json:
      Make Role and RoleMapping public.
    This allows us to see them in the web interface.

--- loopback/server/boot/script.js
+++ loopback/server/boot/script.js
@@ -1,30 +1,35 @@
 module.exports = function(app) {
     var MongoDB = app.dataSources.MongoDB;
 
-    MongoDB.automigrate('Customer', function(err) {
-        if (err) {console.log("automigrate"); throw (err);}
+    MongoDB.autoupdate('Customer', function(err) {
+        if (err) {console.log("autoupdate"); throw (err);}
         var Customer = app.models.Customer;
 
-        Customer.create([
+        Customer.findOrCreate(
+            {where: {username: 'Admin'}},
             {username: 'Admin', email: 'larry@towanda.com', password: 'xyzzy', userId: 0, 'admin': true},
-        ], function(err, users) {
-            if (err) {console.log("Customer.create"); throw (err);}
-            var Role = app.models.Role;
-            var RoleMapping = app.models.RoleMapping;
+            function(err, users) {
+                if (err) {console.log("Customer.create"); throw (err);}
+                var Role = app.models.Role;
+                var RoleMapping = app.models.RoleMapping;
 
-            // Create the admin role.
-            Role.create(
-                {name: 'admin'},
-                function(err, role) {
-                    if (err) {console.log("Customer.create"); throw (err);}
-                    // Make admin.
-                    role.principals.create({
-                        principalType: RoleMapping.USER,
-                        principalId  : users[0].id,
-                    }, function(err, principal) {
-                        if (err) {console.log("role.principals.create"); throw (err);}
+                // Create the admin role.
+                Role.findOrCreate(
+                    {where: {name: 'admin'}},
+                    {name: 'admin'},
+                    function(err, role, created) {
+                        if (err) {console.log("Customer.create"); throw (err);}
+                        if (created) {
+                            // Make admin.
+                            role.principals.create(
+                                {
+                                    principalType: RoleMapping.USER,
+                                    principalId  : users.id,
+                                }, function(err, principal) {
+                                    if (err) {console.log("role.principals.create"); throw (err);}
+                                });
+                        }
                     });
-                });
-        });
+            });
     });
 };

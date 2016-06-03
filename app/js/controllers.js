5b7db11d0c8d423f917824686cbe259fe9726699
Larry Fenske <github@towanda.com>
2016-06-02 20:04:27 -0600

    Accounts work now.
    Retrieve and update loginService.info and accountService.info, e.g. most
    recent account for a customer.
    Getting account selection working was difficult.  Still need a way to
    set it without clicking.
    Prevent duplicate customers and accounts.
    Tweak customer and account models.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -91,20 +91,47 @@ angular.module('checkbook.controllers', ['ui.bootstrap'])
                     'EntryFormController');
             };
 
+            $scope.setAccounted = function(p, acctId) {
+                $scope.accounted = p;
+                if ($scope.accounted) {
+                    accountService.acctId = acctId;
+                    console.log("acctId: "+accountService.acctId);
+                    accountService.get();
+                    if (loginService.info.recent !== acctId) {
+                        loginService.update({recent:acctId});
+                    }
+                }
+            };
+            accountService.setAccounted = $scope.setAccounted;
+
             $scope.setLogged_in = function(p, name, custId) {
                 $scope.logged_in = p;
                 if ($scope.logged_in) {
                     $scope.username = name;
                     loginService.custId = custId;
                     console.log("username: "+$scope.username+", custId: "+loginService.custId);
+                    loginService.get(
+                        function() {
+                            var recent = loginService.info.recent;
+                            if (recent !== "") {
+                                accountService.setAccounted(true, recent);
+                                $scope.accountSelected = recent;
+                            } else {
+                                $scope.accountSelected = "add";
+                            }
+                        });
                 } else {
-                    $scope.accounted = false;
+                    $scope.setAccounted(false);
                 }
             };
-
             $scope.setLogged_in(false);
-
             loginService.setLogged_in = $scope.setLogged_in;
+
+            $scope.accountSelected = "add";
+            $scope.selectAccount = function() {
+                console.log("selectAccount accountSelected: "+JSON.stringify($scope.accountSelected));
+                $scope.setAccounted(true, $scope.accountSelected);
+            };
 
             $scope.setAccountData = function(data) {
                 $scope.accountData = data;
@@ -151,8 +178,20 @@ angular.module('checkbook.controllers', ['ui.bootstrap'])
 
             $scope.createUser = function() {
                 $uibModalInstance.close();
-                loginService.createUser($scope.createUserData);
-                console.log("createUser: "+JSON.stringify($scope.createUserData));
+                loginService.count($scope.createUserData.username)
+                    .then(
+                        function(response) {
+                            if (response.count === 0) {
+                                loginService.createUser($scope.createUserData);
+                                console.log("createUser: "+JSON.stringify($scope.createUserData));
+                            } else {
+                                console.log("createUser: user already exists "+response.count+" time(s)");
+                            }
+                        },
+                        function(response) {
+                            console.log("loginService.count failed: "+JSON.stringify(response));
+                        }
+                    );
             };
 
             $scope.cancel = function() {
@@ -176,10 +215,22 @@ angular.module('checkbook.controllers', ['ui.bootstrap'])
 
             $scope.createAccount = function() {
                 $uibModalInstance.close();
-                accountService.createAccount(
-                    $scope.createAccountData,
-                    function() { $rootScope.getAccountList(); });
-                console.log("createAccount: "+JSON.stringify($scope.createAccountData));
+                accountService.count($scope.createAccountData.acctname)
+                    .then(
+                        function(response) {
+                            if (response.count === 0) {
+                                accountService.createAccount(
+                                    $scope.createAccountData,
+                                    function() { $rootScope.getAccountList(); });
+                                console.log("createAccount: "+JSON.stringify($scope.createAccountData));
+                            } else {
+                                console.log("createAccount: account already exists "+response.count+" time(s)");
+                            }
+                        },
+                        function(response) {
+                            console.log("accountService.count failed: "+JSON.stringify(response));
+                        }
+                    );
             };
 
             $scope.cancel = function() {
--- app/js/services.js
+++ app/js/services.js
@@ -32,12 +32,12 @@ angular.module('checkbook.services', ['ngResource'])
             baseURL) {
 
             this.custId = null;
+            this.access_token = null;
+            this.setLogged_in = function() {};
 
             this.createUser = function(data) {
                 return $resource(baseURL+'/Customers').save(data);
             };
-
-            this.setLogged_in = function() {};
 
             this.login = function(data) {
                 return $resource(baseURL+'/Customers/login').save(data).$promise;
@@ -45,6 +45,34 @@ angular.module('checkbook.services', ['ngResource'])
 
             this.logout = function() {
                 return $resource(baseURL+'/Customers/logout');
+            };
+
+            this.count = function(username) {
+                return $resource(baseURL+'/Customers/count?where='+JSON.stringify({username:username})).get().$promise;
+            };
+
+            this.get = function(cb) {
+                this.info = $resource(baseURL+'/Customers/'+this.custId+'?access_token='+this.access_token).get(
+                    function(response) {
+                        console.log('loginService.get: '+JSON.stringify(response));
+                        if (cb) {
+                            cb();
+                        }
+                    },
+                    function(response) {
+                        console.log('loginService.get: '+JSON.stringify(response));
+                    });
+            };
+
+            this.update = function(data) {
+                this.info = $resource(baseURL+'/Customers/'+this.custId+'?access_token='+this.access_token, null, {'update': {method: 'PUT'}}).update(
+                    data,
+                    function(response) {
+                        console.log('loginService.update: '+JSON.stringify(response));
+                    },
+                    function(response) {
+                        console.log('loginService.update: '+JSON.stringify(response));
+                    });
             };
 
         }])
@@ -57,6 +85,9 @@ angular.module('checkbook.services', ['ngResource'])
             $resource,
             loginService,
             baseURL) {
+
+            this.acctId = null;
+            this.setAccounted = function() {};
 
             this.createAccount = function(data, cb) {
                 data.custId = loginService.custId;
@@ -74,7 +105,6 @@ angular.module('checkbook.services', ['ngResource'])
             };
 
             this.getAccountList = function(cb) {
-                console.log("try getting from "+baseURL+'/Accounts?access_token='+loginService.access_token);
                 this.accountList = $resource(baseURL+'/Accounts?access_token='+loginService.access_token+'&filter='+JSON.stringify({where: {custId: loginService.custId}}))
                     .query(
                         function(response) {
@@ -85,6 +115,31 @@ angular.module('checkbook.services', ['ngResource'])
                             console.log("getAccountList: "+JSON.stringify(response));
                         }
                     );
+            };
+
+            this.count = function(acctname) {
+                return $resource(baseURL+'/Accounts/count?where='+JSON.stringify({custId:loginService.custId,acctname:acctname})).get().$promise;
+            };
+
+            this.get = function() {
+                this.info = $resource(baseURL+'/Accounts/'+this.acctId+'?access_token='+loginService.access_token).get(
+                    function(response) {
+                        console.log('accountService.get: '+JSON.stringify(response));
+                    },
+                    function(response) {
+                        console.log('accountService.get: '+JSON.stringify(response));
+                    });
+            };
+
+            this.update = function(data) {
+                this.info = $resource(baseURL+'/Accounts/'+this.custId+'?access_token='+loginService.access_token, null, {'update': {method: 'PUT'}}).update(
+                    data,
+                    function(response) {
+                        console.log('accountService.update: '+JSON.stringify(response));
+                    },
+                    function(response) {
+                        console.log('accountService.update: '+JSON.stringify(response));
+                    });
             };
 
         }])
